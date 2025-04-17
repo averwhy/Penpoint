5af1c229d3bfcad643fa30d458b0d5f02ecc8525
averyB <averwhy@gmail.com>
2025-04-16 20:29:32 -0400

    Add template page, add other pages, setup nuxt ui

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="app-container">
+    <img src="/penmenpridelogo.png" alt="Penn Men Pride Logo" class="logo-image" />
+    <NuxtRouteAnnouncer />
+  </div>
+</template>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  background-color: #202027;
+}
+
+.app-container {
+  min-height: 100vh;
+  width: 100%;
+  background-color: #202027;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.logo-image {
+  max-width: 80%;
+  height: auto;
+}
+</style>
