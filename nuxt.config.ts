7508c4f168dd05fd54b04f1cdb6af0bb813b8959
averyB <averwhy@gmail.com>
2025-05-31 02:14:36 -0400

    Begin api implementation, progress on db schema

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,31 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxthub/core'],
-  css: ['~/assets/css/main.css']
+  modules: ['@nuxt/ui', '@nuxthub/core', '@sidebase/nuxt-auth'],
+  css: ['~/assets/css/main.css'],
+
+  auth: {
+    isEnabled: true,
+    originEnvKey: 'NUXT_PUBLIC_AUTH_BASE_URL',
+    provider: {
+      type: 'local',
+      endpoints: {
+        signIn: { path: '/login', method: 'post' },
+        signOut: { path: '/logout', method: 'post' },
+        signUp: { path: '/register', method: 'post' },
+        getSession: { path: '/session', method: 'get' }
+      },
+      token: {
+        signInResponseTokenPointer: '/accessToken'
+      },
+      pages: {
+        login: '/login'
+      }
+    },
+    sessionRefresh: {
+      enablePeriodically: true,
+      enableOnWindowFocus: true
+    },
+    globalAppMiddleware: true
+  }
 })
--- server/utils/schema.sql
+++ server/utils/schema.sql
@@ -1,35 +1,68 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
--- Club eboard members and SGA
-CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4();
-    student_id VARCHAR(7) NOT NULL;
-    email TEXT NOT NULL;
-    name TEXT NOT NULL;
-    password_hash TEXT NOT NULL;
-    is_active BOOLEAN NOT NULL DEFAULT true;
-    is_verified BOOLEAN NOT NULL DEFAULT false;
-    refresh_token TEXT NOT NULL;
-    last_login TIMESTAMPTZ NOT NULL DEFAULT now();
-    created_at TIMESTAMPTZ NOT NULL DEFAULT now();
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT now();
+
+-- regular students (They won't be able to login to anything)
+CREATE TABLE students (
+    student_id VARCHAR(7) PRIMARY KEY,
+    email TEXT UNIQUE, -- This and `name` are going to be null by default
+    name TEXT, -------------^^^^^^^^^^^^^^^
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
-CREATE TABLE students (
-    student_id VARCHAR(7) PRIMARY KEY;
-    email TEXT;
-    name TEXT;
-    created_at TIMESTAMPTZ NOT NULL DEFAULT now();
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT now();
-)
+-- Club eboard members and SGA 
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    student_id VARCHAR(7) REFERENCES students(student_id), -- Should/Will match students table
+    email TEXT UNIQUE NOT NULL,
+    name TEXT NOT NULL,
+    password_hash TEXT NOT NULL,
+    refresh_token TEXT,
+    last_login TIMESTAMPTZ NOT NULL DEFAULT now(),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE admins (
+    id UUID PRIMARY KEY REFERENCES users(id),
+    active BOOLEAN NOT NULL DEFAULT true,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE semesters (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    starts TIMESTAMPTZ NOT NULL,
+    ends TIMESTAMPTZ NOT NULL,
+    code VARCHAR(3) NOT NULL -- E.g. A3 (it's snhu's new semester coding system, used to be like FADAY23)
+);
+
+CREATE TABLE clubs (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name TEXT UNIQUE NOT NULL,
+    acronym TEXT UNIQUE NOT NULL,
+    governing_board BOOLEAN NOT NULL DEFAULT false,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE club_users(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    position TEXT NOT NULL,
+    user_id UUID REFERENCES users(id),
+    club_id UUID REFERENCES clubs(id),
+    for_semester UUID REFERENCES semesters(id)
+);
 
 CREATE TABLE events (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4();
-    name TEXT NOT NULL;
-    location TEXT NOT NULL;
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL,
+    location TEXT NOT NULL
     
-)
+);
 
 CREATE TABLE taps (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4();
-    event_id
-)
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    semester_id
+    student_id VARCHAR(7) REFERENCES students(student_id),
+    event_id UUID REFERENCES events(id)
+);
