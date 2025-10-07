90c39ce9ee301a685c64385d84a11c25014a0ec9
avery bertrand <averwhy@gmail.com>
2025-10-07 12:55:08 -0400

    Did you do it? "Yes" What did it cost? "Everything"

--- pages/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="flex min-h-screen w-full flex-col items-center justify-center overflow-hidden bg-[#1e1e22] px-4">
-    <h1 class="mb-8 text-center text-4xl font-semibold text-white">Penmen Pride</h1>
-    <NuxtRouteAnnouncer />
-  </div>
-</template>
--- server/api/[trpc].ts
+++
@@ -1,9 +0,0 @@
-import { createTRPCNuxtHandler } from "trpc-nuxt/server";
-import { createTRPCContext } from "~/server/trpc/init";
-import { appRouter } from "~/server/trpc/routers";
-
-export default createTRPCNuxtHandler({
-	endpoint: "/api/trpc",
-	router: appRouter,
-	createContext: createTRPCContext,
-});
