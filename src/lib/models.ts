202713ebc6d235edc1a53785c45f7b3785bf031b
Nick <nick@ostra.gg>
2025-11-10 13:01:32 -0500

    fix: cap most user-controlled strings to reasonable length limits

--- src/lib/models.ts
+++ src/lib/models.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const Password = z
     .string()
     .min(8, "Password must be at least 8 characters.")
-    .max(256, "Password must be at most 256 characters.")
+    .max(70, "Password must be at most 70 characters.")
     .refine(val => /[A-Z]/.test(val), {
         error: "Password must contain at least one uppercase letter.",
     })
@@ -15,30 +15,30 @@ export const Password = z
     });
 export type Password = z.infer<typeof Password>;
 
+export const StudentId = z.string().regex(/^\d{7}$/);
+export type StudentId = z.infer<typeof StudentId>;
+
 export const Login = z.object({
-    email: z.email(),
+    email: z.email().max(100),
     _password: z.string(), // don't use password schema for logging in, it's used on registration
 });
 export type Login = z.infer<typeof Login>;
 
 export const Registration = z.object({
-    name: z.string(),
-    email: z.email(),
-    password: Password, // password schema should only be used here
-    studentid: z.string(),
-    reason: z.string(),
+    name: z.string().max(100),
+    email: z.email().max(100),
+    // password: Password, // password schema should only be used here
+    student_id: StudentId,
+    reason: z.string().max(10000),
 });
 export type Registration = z.infer<typeof Registration>;
 
 // db schemas
 
-export const StudentId = z.string().regex(/^\d{7}$/);
-export type StudentId = z.infer<typeof StudentId>;
-
 export const Student = z.object({
     student_id: StudentId,
-    email: z.email(),
-    name: z.string(),
+    email: z.email().max(100),
+    name: z.string().max(100),
     created_at: z.coerce.date(),
     updated_at: z.coerce.date(),
 });
@@ -47,10 +47,10 @@ export type Student = z.infer<typeof Student>;
 export const User = z.object({
     id: z.uuid(),
     student_id: StudentId,
-    email: z.email(),
-    name: z.string(),
+    email: z.email().max(100),
+    name: z.string().max(100),
     role: z.enum(["unapproved", "club", "sga", "admin"]),
-    request_reason: z.string(),
+    request_reason: z.string().max(10000),
     last_login: z.coerce.date(),
     created_at: z.coerce.date(),
     updated_at: z.coerce.date(),
@@ -69,8 +69,8 @@ export type Semester = z.infer<typeof Semester>;
 
 export const Club = z.object({
     id: z.uuid(),
-    name: z.string(),
-    acronym: z.string(),
+    name: z.string().max(100),
+    acronym: z.string().max(100),
     governing_board: z.boolean(),
     created_at: z.coerce.date(),
     updated_at: z.coerce.date(),
@@ -79,7 +79,7 @@ export type Club = z.infer<typeof Club>;
 
 export const ClubUser = z.object({
     id: z.uuid(),
-    position: z.string(),
+    position: z.string().max(100),
     user_id: z.uuid(),
     club_id: z.uuid(),
     semester_id: z.uuid(),
@@ -90,8 +90,8 @@ export type ClubUser = z.infer<typeof ClubUser>;
 
 export const Event = z.object({
     id: z.uuid(),
-    name: z.string(),
-    location: z.string(),
+    name: z.string().max(100),
+    location: z.string().max(100),
     point_value: z.int(),
     starts_at: z.coerce.date(),
     ends_at: z.coerce.date(),
@@ -109,8 +109,8 @@ export const Tap = z.object({
 export type Tap = z.infer<typeof Tap>;
 
 export const PointCheck = z.object({
-    student_id: StudentId
-})
+    student_id: StudentId,
+});
 export type PointCheck = z.infer<typeof PointCheck>;
 
 // API Response schemas
