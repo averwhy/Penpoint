ee815c6fed77ebe8a1e5c7f2fc6c572b04abce22
avery <averwhy@gmail.com>
2026-02-28 16:14:32 -0500

    Remove test config

--- vite.config.ts
+++ vite.config.ts
@@ -5,34 +5,6 @@ import { playwright } from '@vitest/browser-playwright'
 
 export default defineConfig({
     plugins: [tailwindcss(), sveltekit()],
-    test: {
-        expect: { requireAssertions: true },
-        projects: [
-            {
-                extends: "./vite.config.ts",
-                test: {
-                    name: "client",
-                    browser: {
-                        enabled: true,
-                        provider: playwright(),
-                        instances: [{ browser: "chromium" }],
-                    },
-                    include: ["src/**/*.svelte.{test,spec}.{js,ts}"],
-                    exclude: ["src/lib/server/**"],
-                    setupFiles: ["./vitest-setup-client.ts"],
-                },
-            },
-            {
-                extends: "./vite.config.ts",
-                test: {
-                    name: "server",
-                    environment: "node",
-                    include: ["src/**/*.{test,spec}.{js,ts}"],
-                    exclude: ["src/**/*.svelte.{test,spec}.{js,ts}"],
-                },
-            },
-        ],
-    },
     server: {
         fs: {
             allow: ["uploads"],
